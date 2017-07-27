bb206777458335b281875e4e063161c939995fad
Tyler Reitz <tdreitz@gmail.com>
2017-07-27 11:54:12 -0700

    add base case

--- src/app.js
+++ src/app.js
@@ -4,8 +4,18 @@ inquirer
   .prompt([
     {
       type: 'input',
-      message: 'Hæ wuts up?',
-      name: 'greeting'
+      message: 'What is the name of the company',
+      name: 'company'
+    },
+    {
+      type: 'input',
+      message: 'What is the name of the addressee',
+      name: 'addressee'
+    },
+    {
+      type: 'input',
+      message: 'Where is the listing from',
+      name: 'listing'
     }
   ])
   .then((answer) => {
