bed01e13029e620e33869a344112531b6ebacb00
Tyler Reitz <tdreitz@gmail.com>
2017-07-28 16:40:26 -0700

    add copy-paste w/ clipboardy

--- src/app.js
+++ src/app.js
@@ -1,47 +1,43 @@
-const inquirer = require('inquirer')
-const util = require('util')
-const fs = require('fs')
-const path = require('path')
+const inquirer = require("inquirer");
+const util = require("util");
+const fs = require("fs");
+const path = require("path");
+const clipboardy = require("clipboardy");
 
-const templatePath = path.join(__dirname, "templates")
-const readdir = util.promisify(fs.readdir)
-const readfile = util.promisify(fs.readFile)
+const templatePath = path.join(__dirname, "templates");
+const readdir = util.promisify(fs.readdir);
+const readfile = util.promisify(fs.readFile);
 
-readdir(
-    templatePath, 'utf8'
-  )
+readdir(templatePath, "utf8")
   .then(templates => {
-    return inquirer
-      .prompt([
-        {
-          type: 'list',
-          message: 'Which template would you like to use',
-          choices: templates,
-          name: 'template'
-        }
-      ])
+    return inquirer.prompt([
+      {
+        type: "list",
+        message: "Which template would you like to use",
+        choices: templates,
+        name: "template"
+      }
+    ]);
   })
-  .then(({template}) => {
-    return require(
-      path.join(__dirname, 'templates', template)
-    )
+  .then(({ template }) => {
+    return require(path.join(__dirname, "templates", template));
   })
-  .then(({slugs, letter}) => {
-    let questions = []
+  .then(({ slugs, letter }) => {
+    let questions = [];
 
     for (var slug in slugs) {
       questions.push({
-        type: 'input',
+        type: "input",
         message: slugs[slug],
         name: slug
-      })
+      });
     }
 
-    inquirer
-      .prompt(questions)
+    return inquirer.prompt(questions)
       .then(answers => {
-        console.log(letter(answers))
-      });
-    
-  })
-  .catch(err => console.log(err))
+        clipboardy.writeSync(letter(answers))
+      })
+      .catch(err => console.log(err))
+      
+    })
+    .catch(err => console.log(err));
