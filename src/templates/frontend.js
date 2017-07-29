d69667010625d0611147f9dc68949a382be62980
Tyler Reitz <tdreitz@gmail.com>
2017-07-28 21:56:47 -0700

    minor tweaks & updates

--- src/templates/frontend.js
+++ src/templates/frontend.js
@@ -5,19 +5,19 @@ const general = {
     listing: 'Where is the listing from'
   },
   letter: function({company, addressee, listing}) {
-    return `Hello ${addressee},
+    return `Hey ${addressee},
 
 I’m a Los Angeles (DTLA) based UX Designer, turned Front-End Developer, turned Fullstack Engineer with ~3 years experience working with JS libs/frameworks and 1+ year into completely focusing on the React ecosystem. 
 
 I can work with all of the following technologies (and have taught them to students): Redux, React Native, React Router, SSR, REST APIs, GraphQL, Webpack, Jest/Mocha, Styled Components, CSS-in-JS, CSS Modules and anything else the community can come up with. 
 
-I’ve also tried to remained as framework-agnostic as possible by keeping focus on competing frameworks, the native language of the web itself, and its associated technologies.
+I’ve also tried to remain as framework-agnostic as possible by keeping focus on competing frameworks, the native language of the web itself, and its associated technologies.
 
 I’m reaching out to you today on ${listing} because I think ${company}’s core mission aligns perfectly with my core technical competencies and professional interests.
 
 If your interest is at all piqued by what you’ve read so far feel free to reach out anytime that’s convenient.
 
-Looking Forward To It,
+Best,
 Tyler Reitz
 `;
   }
--- src/templates/fullstack.js
+++ src/templates/fullstack.js
@@ -7,15 +7,15 @@ const general = {
   letter: function({company, addressee, listing}) {
     return `Hey ${addressee},
     
-I’m Los Angeles (DTLA) based UX Designer, turned Front-End Developer, turned Full-Stack Engineer who's worked for a wide variety of clients and companies—from the scrappiets-of-startups on up to the top of the Fortune 500 (and everywhere in between).
+I’m a Los Angeles (DTLA) based UX Designer, turned Front-End Developer, turned Full-Stack Engineer who's worked for a wide variety of clients and companies—from the scrappiest-of-startups on up to the top of the Fortune 500 (and everywhere in between).
 
-My expertise runs anywhere Javascript does, beginning with client-side technologies like React/React-Native + CSS-in-JS, extends to creating scalable backends with REST apis, GraphQL, and Node server-side frameworks like Express, handles data modeling and persistence with SQL/NoSQL db’s like MySQL and Mongo, and ultimately, ends with a CI/CD delivery approach across multiple platforms and devices.
+My expertise runs anywhere Javascript does, beginning with client-side technologies like React/React-Native + CSS-in-JS, extends to creating scalable backends with REST apis, GraphQL, and Node server-side frameworks like Express, handles data modeling and persistence with SQL/NoSQL db’s like MySQL and Mongo, and ultimately, ends with a test-first and CI/CD delivery approach across multiple platforms and devices.
 
 I’m reaching out to you today on ${listing} because I think ${company}’s core mission aligns perfectly with my core technical competencies and professional interests.
 
 If your interest is at all piqued by what you’ve read so far feel free to reach out anytime that’s convenient.
 
-Looking Forward To It,
+Best,
 Tyler Reitz
 `
   }
